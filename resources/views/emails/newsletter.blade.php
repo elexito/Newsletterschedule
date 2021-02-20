b4edcd508b2f41b48c2a13bfd5406c31558e0c95
emlycool <joshua.moshood@gmail.com>
2021-02-20 19:10:54 +0100

    laravel install

---
+++ resources/views/emails/newsletter.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# TuesDays of the month
+
+
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
